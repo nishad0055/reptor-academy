3175a4d30079ea6988735c199538593386ceac51
nishadahmed5 <nishatahmed650@gmail.com>
2022-10-26 17:26:36 +0600

    course prevew page set

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaStar } from 'react-icons/fa'
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 import CourseDetails from '../CourseDetails/CourseDetails';
 
@@ -16,11 +16,13 @@ const CourseCard = ({course}) => {
                     {title}
                     <div className="badge badge-secondary">{rating.badge} </div>
                     </h2>
-                    <h5 className='m-0 p-0' >Instructor:{instructor.name}</h5>
+                    <div>
+                    <h5 className='mb-0' >Instructor:{instructor.name}</h5>
                     <h5 className='' ><small>{instructor.title}</small></h5>
+                    </div>
                     <div className="card-actions">
                     <div className="badge badge-outline text-yellow-600 ">{rating.number}</div> 
-                    <div className="badge badge-outline text-yellow-500"> <FaStar  ></FaStar> <FaStar></FaStar><FaStar></FaStar><FaStar></FaStar><FaStar></FaStar></div>
+                    <div className="badge badge-outline text-yellow-500"> <FaStar  ></FaStar> <FaStar></FaStar><FaStar></FaStar><FaStar></FaStar> <FaStarHalfAlt/> </div>
                     </div>
                 </div>
            </div> 
--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,16 +1,42 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
+import { FaStar,  FaStarHalfAlt } from 'react-icons/fa'
 
 
 const CourseDetails = () => {
     const courseDetails = useLoaderData();
     const {title, details, image_url , instructor, duration , lecture, chapter, rating} = courseDetails;
-    console.log(details)
+   
     return (
         <div  className='container mx-auto my-5 flex'>
-          {
-            <h3>{title}</h3>
-          }
+          <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure><img className='w-[400px]' src= {image_url} alt="/"/></figure>
+            <div className="card-body">
+                
+                <h2 className="card-title">{title}</h2>
+                <div>
+                     <p className='text-gray-500'><span> Course Duration: {duration}</span> | <span>{chapter} Chapter</span> | <span>{lecture} Lecture.</span> </p>
+                      <div className='flex justify-start'>
+                         <p className='text-gray-500' >ratings: {rating.number}</p>
+                        <div className='flex text-yellow-500'>
+                        <FaStar/>
+                        <FaStar/>
+                        <FaStar/>
+                        <FaStarHalfAlt/>
+                        </div>
+                      </div>
+                </div>
+                <p>{details}</p>
+                <div>
+                <p className='mb-0' >Instructor:{instructor.name}</p>
+                 <p className='' ><small>{instructor.title}</small></p>
+                </div>
+                <div className="card-actions justify-end">
+                <button className="btn btn-primary"> <Link>DownLoad Pdf</Link> </button>   
+                <button className="btn btn-primary"> <Link>Get Premium Access</Link> </button>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
